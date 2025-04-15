f92339486329c17390a715df111daee4c688feab
Susie Kim <35243112+susiekim101@users.noreply.github.com>
2025-04-14 23:06:28 -0700

    Initial commit

---
+++ WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Susie Kim on 4/13/25.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
