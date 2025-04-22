e7978925748157bb9e372412a00d24e2b4cdf0c5
Susie Kim <35243112+susiekim101@users.noreply.github.com>
2025-04-22 08:53:45 -0700

    Logic and variable set up for user input

--- BetterRest/BetterRest/ContentView.swift
+++ BetterRest/BetterRest/ContentView.swift
@@ -8,11 +8,31 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var wakeUp = Date.now // current data & time
+    @State private var wakeUp = Date.now
+    @State private var sleepAmount = 8.0
+    @State private var coffeeAmount = 1
     
     var body: some View {
-        // Text(Date.now, format: .dateTime.hour().minute())
-        Text(Date.now.formatted(date: .long, time: .shortened))
+        NavigationStack {
+            VStack {
+                Text("When do you want to wake up?")
+                    .font(.headline)
+                
+                DatePicker("Please enter a time", selection: $wakeUp, displayedComponents: .hourAndMinute) // what TIME someone wants to wake up
+                    .labelsHidden() // don't want the actual text label
+                
+                Text("Desired amount of sleep")
+                    .font(.headline)
+                
+                Stepper("\(sleepAmount.formatted()) hours", value: $sleepAmount, in: 4...12, step: 0.25)
+                
+                Text("Daily coffee intake")
+                    .font(.headline)
+                
+                Stepper("\(coffeeAmount) coffee", value: $coffeeAmount, in: 1...20, step: 1)
+            }
+            
+        }
     }
 }
 
