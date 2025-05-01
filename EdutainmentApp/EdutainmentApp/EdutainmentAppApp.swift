78ecc17c5dc5fc96d04cbb8b798da6dcafe89d36
Susie Kim <35243112+susiekim101@users.noreply.github.com>
2025-05-01 09:23:54 -0700

    landing page ui, logic for game page

---
+++ EdutainmentApp/EdutainmentApp/EdutainmentAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  EdutainmentAppApp.swift
+//  EdutainmentApp
+//
+//  Created by Susie Kim on 5/1/25.
+//
+
+import SwiftUI
+
+@main
+struct EdutainmentAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
