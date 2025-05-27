ffde8ebb3c90c085110e2a7aebbeb3fbbc77cc5c
Susie Kim <35243112+susiekim101@users.noreply.github.com>
2025-05-26 19:49:06 -0700

    utilize query and swiftdata

--- Bookworm/Bookworm/ContentView.swift
+++ Bookworm/Bookworm/ContentView.swift
@@ -17,7 +17,6 @@ struct ContentView: View {
     
     var body: some View {
         NavigationStack {
-            Text("Count: \(books.count)")
             List {
                 ForEach(books) { book in
                     NavigationLink(value: book) {
