838a62f0cee1501648c2d76fdc89ba3342c9cc15
Susie Kim <35243112+susiekim101@users.noreply.github.com>
2025-06-04 23:08:55 -0700

    add voiceover for flag descriptions

--- GuessTheFlagApp/GuessTheFlag/ContentView.swift
+++ GuessTheFlagApp/GuessTheFlag/ContentView.swift
@@ -33,6 +33,20 @@ struct ContentView: View {
     
     @State private var score = 0
     
+    let labels = [
+        "Estonia": "Flag with three horizontal stripes. Top stripe blue, middle stirpe black, bottom stripe white.",
+        "France": "Flag with three vertical stripes. Left stripe blue, middle stripe white, right stripe red.",
+        "Germany": "Flag with three horizontal stripes. Top stripe black, middle stripe red, bottom stripe gold.",
+        "Ireland": "Flag with three vertical stripes. Left stripe green, middle stripe white, right stripe orange.",
+        "Italy": "Flag with three vertical stripes. Left stripe green, middle stripe white, right stripe red.",
+        "Nigeria": "Flag with three vertical stripes. Left stripe green, middle stripe white, right stripe green.",
+        "Poland": "Flag with two horizontal stripes. Top stripe white, bottom stripe red.",
+        "Spain": "Flag three horizontal stripes. Top thin stripe red, middle thick stripe is gold with crest on the left, bottom thin stripe red.",
+        "UK": "Flag with overlapping red and white crosses, both straight and diagonally, one a blue background.",
+        "Ukraine": "Flag with two horizontal stripes. Top stripe blue, bottom stripe yellow",
+        "US": "Flag with many red and white stripes, with white stars on a blue background in the top-left corner."
+    ]
+    
     var body: some View {
         ZStack {
             RadialGradient(stops: [
@@ -75,6 +89,7 @@ struct ContentView: View {
                         .scaleEffect(tappedButton == nil ||
                                      tappedButton == number ? 1 : 0.75)
                         .animation(.easeInOut(duration: 0.6), value: tappedButton)
+                        .accessibilityLabel(labels[countries[number], default: "Unknown flag"])
                     }
                 }
                 .frame(maxWidth: .infinity)
